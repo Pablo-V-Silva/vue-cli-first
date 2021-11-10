12fab740ab198f0c0caf70fc4843890a9d8bc934
Pablo-V-Silva <booleanpablosilva@gmail.com>
2021-11-10 14:41:14 +0100

    Exercize complete

---
+++ vue-first/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <Title msg="La mia prima app con Vue CLI!"/>
+  </div>
+</template>
+
+<script>
+import Title from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Title
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
